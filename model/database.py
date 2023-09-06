79652316bb70ed38d87550a09beaa680980b3537
Nerakc <nerakc.mat@gmail.com>
2023-09-06 11:09:33 +0200

    database fix (still hate Pypi)

--- model/database.py
+++ model/database.py
@@ -1,22 +1,28 @@
+from typing import List
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass 
+
+
 
 anime_openings = db.Table("anime_openings",
-    Column('anime_id', Integer, ForeignKey('anime.id'), primary_key=True),
-    Column('opening_id', Integer, ForeignKey('opening.id'), primary_key=True)
-                          )
-                        
+    Base.metadata,
+    Column('anime_id', ForeignKey('anime.id'), primary_key=True),
+    Column('opening_id', ForeignKey('opening.id'), primary_key=True)
+)
+
+
 class Artist(Base):
     __tablename__ = 'artist'
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
-    openings = relationship('Opening', backref='artist')
+    openings = relationship('opening', backref='artist')
     __table_args__ = (db.UniqueConstraint('name', name='_artist_uc'),)
     def __repr__(self):
         return f"<Artist#{self.id} {self.name}>"
@@ -34,18 +40,19 @@ class Song(Base):
     id = Column(Integer, primary_key=True)
     song_title = Column(String(255), nullable=False)
     artist_id = Column(Integer, ForeignKey('artist.id'), nullable=False)
-    opening = relationship('Opening', backref='song')
+    opening_id = Column(Integer, ForeignKey('opening.id'), nullable=False)
     spotify_url = Column(String(255), nullable=False)
     votes = Column(Integer, nullable=False, default=0)
     # __table_args__ = (db.UniqueConstraint('id', 'song_title', name='_song_uc'),)
     def __repr__(self):
         return f"<Song#{self.id} {self.song_title}>"
 
+
 class Anime(Base):
     __tablename__ = 'anime'
     id = Column(Integer, primary_key=True)
     anime_title = Column(String(255), nullable=False)
-    openings = relationship('Opening', secondary=anime_openings, backref='anime')
+    openings: Mapped[List[Opening]]= relationship(secondary=anime_openings)
     __table_args__ = (db.UniqueConstraint('id', 'anime_title', name='_anime_uc'),)
     def __repr__(self):
         return f"<Anime#{self.id} {self.anime_title}>"
