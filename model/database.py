613a5cb26635f6009b2102968292dc793f5c33cb
Nerakc <nerakc.mat@gmail.com>
2023-12-27 16:53:10 +0100

    db change for sqlite reasons

--- model/database.py
+++ model/database.py
@@ -110,7 +110,7 @@ class Sync(db.Model):
     last_synced_at = db.Column(db.DateTime, nullable=False)
 
 anime_opening = db.Table(
-    'anime_opening',
+    'anime_openings',
     db.Column('anime_id', db.Integer, db.ForeignKey('anime.id'), primary_key=True),
     db.Column('opening_id', db.Integer, db.ForeignKey('opening.id'), primary_key=True),
     db.Column('episodes', db.String(128), nullable=False)
