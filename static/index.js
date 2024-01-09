08efdc9e1acea9e7126d2dcf2d3bd4a942bc8b88
Nerakc <nerakc.mat@gmail.com>
2024-01-10 00:15:18 +0100

    added basics of popup

--- static/index.js
+++ static/index.js
@@ -58,7 +58,7 @@ $().ready(function () {
                 data: "op_title",
                 render: (data, type, row, meta) => {
                     out = `<div class='popup d-flex flex-row justify-content-between'>`
-                    button = `<button class='btn btn-sm btn-primary' onclick='popup-${row.anime_id}'>Change</button>`
+                    button = `<button id='popup-btn-${row.anime_id}' class='btn btn-sm btn-primary popup-btn'>Change</button>`
                     if (row.op_uri == null)
                         out += data
                     else
@@ -155,5 +155,96 @@ $().ready(function () {
             },
         ]
     });
+
+
+    $('body').on('click', '.popup-btn', function () {
+        console.log('clicked');
+        //create a popup with selection of songs from spotify (get from our api) and an input field submitting your own link
+        let id = $(this).attr('id').split('-')[2];
+        let row = dataTable.fnGetData($(this).closest('tr'));
+        let popup = `<div class='popup-content'>`;
+        popup += `<div class='popup-header'>`;
+        popup += `<h3>${row.title}</h3>`;
+        popup += `<h5>${row.op_title}</h5>`;
+        popup += `<button id='popup-close-btn' class='btn btn-close'>Close</button>`; // Add this line
+        popup += `</div>`;
+        popup += `<div class='popup-body'>`;
+        popup += `<div class='popup-body-left'>`;
+        popup += `<iframe src='https://open.spotify.com/embed/track/${row.op_uri.split(':').slice(-1)}' width='300' height='380' frameborder='0' allowtransparency='true' allow='encrypted-media'></iframe>`;
+        popup += `</div>`;
+        popup += `<div class='popup-body-right'>`;
+        popup += `<h4>Search for song</h4>`;
+        popup += `<input type='text' class='form-control' id='popup-search-input' placeholder='Search for song'>`;
+        popup += `<div id='popup-search-results'></div>`;
+        popup += `<h4>Or submit your own link</h4>`;
+        popup += `<input type='text' class='form-control' id='popup-link-input' placeholder='Spotify link'>`;
+        popup += `</div>`;
+        popup += `</div>`;
+        popup += `<div class='popup-footer'>`;
+        popup += `<button id='popup-submit-btn' class='btn btn-primary'>Submit</button>`;
+        popup += `</div>`;
+        popup += `</div>`;
+        $('#popup').html(popup);
+        $('#popup').show();
+
+        $('#popup-submit-btn').on('click', () => {
+            let link = $('#popup-link-input').val();
+            if (link != '') {
+                $.ajax({
+                    url: '/api/mal/updateOp/' + id + '/' + link,
+                    type: 'GET',
+                    success: function (data) {
+                        $('#popup').hide();
+                        dataTable.fnReloadAjax();
+                    },
+                    error: function (data) {
+                        console.log(data);
+                    },
+                    async: false
+                });
+            }
+        });
+
+        $('#popup-search-input').on('keyup', () => {
+            let query = $('#popup-search-input').val();
+            if (query != '') {
+                $.ajax({
+                    url: '/api/spotify/search/' + query,
+                    type: 'GET',
+                    success: function (data) {
+                        let results = data.tracks.items;
+                        let out = '';
+                        for (let i = 0; i < results.length; i++) {
+                            out += `<div class='popup-search-result' data-uri='${results[i].uri}'>`;
+                            out += `<div class='popup-search-result-left'>`;
+                            out += `<img src='${results[i].album.images[2].url}' alt='album cover'>`;
+                            out += `</div>`;
+                            out += `<div class='popup-search-result-right'>`;
+                            out += `<h5>${results[i].name}</h5>`;
+                            out += `<p>${results[i].artists[0].name}</p>`;
+                            out += `</div>`;
+                            out += `</div>`;
+                        }
+                        $('#popup-search-results').html(out);
+                        $('.popup-search-result').on('click', function () {
+                            $('#popup-link-input').val($(this).attr('data-uri'));
+                        });
+                    },
+                    error: function (data) {
+                        console.log(data);
+                    },
+                    async: false
+                });
+            }
+        });
+
+
+        $('#popup-close-btn').on('click', () => {
+            $('#popup').hide();
+        });
+
+
+    });
+
 
 });
